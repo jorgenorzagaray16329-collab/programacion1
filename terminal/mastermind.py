1cde9b82147db5608a9adf681799c7b394c4dc98
juvee <juventino.lopez248547@potros.itson.edu.mx>
2025-11-11 08:56:52 -0700

    Mastermind Terminado.

--- terminal/mastermind.py
+++ terminal/mastermind.py
@@ -32,6 +32,7 @@ def mostrar_colores_disponibles():
 
 def obtener_intento_usuario():
     while True:
+        mostrar_colores_disponibles()
         intento = input(f"Ingresa tu código de {LONGITUD_CODIGO} letras:\n").upper().strip()
 
         if len(intento) != LONGITUD_CODIGO:
@@ -51,5 +52,81 @@ def obtener_intento_usuario():
 
 def evaluar_intento(intento,codigo_secreto):
     #Evalua el intento y devuelve las pistas 
-    return #posicion correcta, color correcto
+    posicion_correcta =0
+    color_correcto = 0
+
+    intento_copia = list(intento)
+    secreto_copia = list(codigo_secreto)
+
+    for i in range(LONGITUD_CODIGO-1,-1,-1):
+        if intento_copia[i] == secreto_copia[i]:
+            posicion_correcta+=1
+            intento_copia.pop(i)
+            secreto_copia.pop(i)
+
+    for letra in intento_copia:
+        if letra in secreto_copia:
+            color_correcto+=1
+            secreto_copia.remove(letra)
+
+    return posicion_correcta, color_correcto
         
+
+def mostrar_tablero(intentos_pasados,pistas_pasadas):
+    """Mostrar el historial de intentos con sus pistas"""
+    print("\n--- Tablero del juego ---")
+    for i in range(len(intentos_pasados)):
+        intento_str=""
+        for letra in intentos_pasados[i]:
+            intento_str += COLORES_POSIBLES[letra] + Style.RESET_ALL+" "
+
+        #Pistas
+        pistas_str = (Fore.GREEN 
+                      + f"{pistas_pasadas[i][0]}P "
+                      + Fore.YELLOW
+                      + f"{pistas_pasadas[i][1]}C "
+                      + Style.RESET_ALL                      
+                      )
+        print(f"Intento {i+1}: {intento_str} | {pistas_str} ")
+        print("-"*20)
+
+
+def jugar():
+    init(autoreset=True)
+
+    print(Fore.CYAN+Style.BRIGHT+"="*40)
+    print(Fore.CYAN+Style.BRIGHT+" Bienvenido a Mastermind de colores   ")
+    print(Fore.CYAN+Style.BRIGHT+"="*40 + "\n")
+
+    codigo_secreto = generar_codigo()
+
+    intentos_restantes = MAX_INTENTOS
+    intentos_pasados = []
+    pistas_pasadas = []
+
+    while intentos_restantes > 0:
+        print(f"Te quedan {Fore.YELLOW+Style.BRIGHT} {intentos_restantes} {Style.RESET_ALL} intentos")
+
+        intento_actual = obtener_intento_usuario()
+
+        pc,cc = evaluar_intento(intento_actual,codigo_secreto)
+    
+        #Guardar el historial de intentos y pistas
+        intentos_pasados.append(intento_actual)
+        pistas_pasadas.append((pc,cc))
+
+        mostrar_tablero(intentos_pasados,pistas_pasadas)
+
+        if pc == LONGITUD_CODIGO:
+            print(Fore.GREEN+Style.BRIGHT+"Felicidades! !Decifraste el código secreto!")
+            break
+
+        intentos_restantes-=1
+
+    if intentos_restantes == 0:
+        print(Fore.RED+Style.BRIGHT+ "\n!Oh no! Te quedaste sin intentos.")
+        secreto_str= " ".join(codigo_secreto)
+        print(f"El codigo secreto era "+secreto_str)
+
+
+jugar()
